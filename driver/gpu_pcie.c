9bc4d4acdd90e0c0a54a1aff74a1882c8ff1e073
David Ventura <davidventura27@gmail.com>
2024-05-13 16:58:44 +0200

    locate BAR#0 address

--- driver/gpu_pcie.c
+++ driver/gpu_pcie.c
@@ -10,8 +10,30 @@ MODULE_LICENSE("GPL");
 
 static struct class *gpu_class;
 
+typedef struct GpuState {
+	struct pci_dev *pdev;
+} GpuState;
+
 static int gpu_probe(struct pci_dev *pdev, const struct pci_device_id *id) {
+	int bars;
+	unsigned long mmio_start, mmio_len;
+	static u8 __iomem *hwmem; /* Memory pointer for the I/O operations */
 	pr_info("called probe");
+
+
+	// create a bitfield of the available BARs
+	bars = pci_select_bars(pdev, IORESOURCE_MEM);
+	pci_enable_device_mem(pdev);
+
+	// claim ownership of the address space for each BAR in the bitfield
+	pci_request_region(pdev, bars, "gpu-pci");
+
+	mmio_start = pci_resource_start(pdev, 0);
+	mmio_len = pci_resource_len(pdev, 0);
+
+	// map physical address to virtual
+	hwmem = ioremap(mmio_start, mmio_len);
+	pr_info("mmio starts at 0x%lx; hwmem 0x%px", mmio_start, hwmem);
 	return 0;
 };
 
