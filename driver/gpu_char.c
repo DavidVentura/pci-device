c372bcaffa85282726a725254738aeabe2eabe0d
David Ventura <davidventura27@gmail.com>
2024-05-23 19:17:21 +0200

    unsure why copy is necessary

--- driver/gpu_char.c
+++ driver/gpu_char.c
@@ -25,23 +25,31 @@ void setup_dma(GpuState* gpu, u8 dir, u32 src, u32 dst, u32 len) {
 	write_reg(gpu, 1, 	REG_DMA_START);
 }
 
-static struct page** pin(const char* buf, size_t count, loff_t offset) {
+static struct page** pin(const char* buf, size_t count, loff_t offset, u32* pages_pinned) {
 	unsigned long user_addr = (unsigned long)buf;
 	unsigned long aligned_addr = user_addr & ~(PAGE_SIZE - 1);
 	unsigned long poffset = user_addr - aligned_addr;
 	int nr_pages = DIV_ROUND_UP(poffset + count, PAGE_SIZE);  // Calculate the total number of pages
 	struct page **pages = kvmalloc_array(nr_pages, sizeof(struct page *), GFP_KERNEL);
-	int pages_pinned = pin_user_pages_fast(aligned_addr, nr_pages, FOLL_LONGTERM, pages);
-	pr_info("pinned %d\n", pages_pinned);
+	*pages_pinned = pin_user_pages_fast(aligned_addr, nr_pages, FOLL_LONGTERM, pages);
+	pr_info("pinned %d\n", *pages_pinned);
 	return pages;
+}
+static void unpin(struct page** pages, u32 count) {
+	for(u32 i=0; i<count; i++) {
+		unpin_user_page(pages[i]);
+	}
+	kvfree(pages);
 }
 
 static ssize_t gpu_fb_read(struct file *file, char __user *buf, size_t count, loff_t *offset) {
+	u32 pages_pinned;
 	GpuState *gpu = (GpuState*) file->private_data;
-
-	struct page **pages = pin(buf, count, *offset);
+	struct page **pages = pin(buf, count, *offset, &pages_pinned);
 	dma_addr_t dma_addr = dma_map_single(&gpu->pdev->dev, buf, count, DMA_FROM_DEVICE);
+
 	irq_fired = 0;
+
 	setup_dma(gpu, DIR_GPU_TO_HOST, *offset, dma_addr, count);
 	if (wait_event_interruptible(wq, irq_fired != 0)) {
 		pr_info("interrupted");
@@ -49,22 +57,35 @@ static ssize_t gpu_fb_read(struct file *file, char __user *buf, size_t count, lo
 		return -ERESTARTSYS;
 	}
 	dma_unmap_single(&gpu->pdev->dev, dma_addr, count, DMA_TO_DEVICE);
-	kvfree(pages);
+	unpin(pages, pages_pinned);
 	return count;
 }
 
 static ssize_t gpu_fb_write(struct file *file, const char __user *buf, size_t count, loff_t *offset) {
 	GpuState *gpu = (GpuState*) file->private_data;
-	struct page **pages = pin(buf, count, *offset);
+	u32 pages_pinned;
+	struct page **pages = pin(buf, count, *offset, &pages_pinned);
 	dma_addr_t dma_addr;
 	u8* kbuf = kmalloc(count, GFP_KERNEL);
 
 	irq_fired = 0;
 
 	copy_from_user(kbuf, buf, count);
-	pr_info("kbuf[0] %d, kbuf[1] %d\n", kbuf[0], kbuf[1]);
+
+	// mapping the userspace buffer directly panics:
+	// BUG: unable to handle page fault for address: ffffdef5e8240900
+	// #PF: supervisor read access in kernel mode
+	// #PF: error_code(0x0000) - not-present page
+	//dma_addr = dma_map_single(&gpu->pdev->dev, buf, count, DMA_TO_DEVICE);
+
+	// "works" == no crash, but buffer is not written
+	dma_addr = dma_map_page(&gpu->pdev->dev, pages[0], 0 /*offset*/, count, DMA_TO_DEVICE);
+	pr_info("dma addr (pages): 0x%llx\n", dma_addr);
+	pr_info("kbuf %px ubuf %px\n", kbuf, buf);
+
+	// works, but needs a memcpy
 	dma_addr = dma_map_single(&gpu->pdev->dev, kbuf, count, DMA_TO_DEVICE);
-	pr_info("DMA addr is - 0x%llx\n", dma_addr);
+	pr_info("dma addr (kbuf): 0x%llx\n", dma_addr);
 	pr_info("setup DMA! - user: 0x%px, offset: 0x%llx\n", buf, *offset);
 	setup_dma(gpu, DIR_HOST_TO_GPU, dma_addr, *offset, count);
 
@@ -72,9 +93,10 @@ static ssize_t gpu_fb_write(struct file *file, const char __user *buf, size_t co
 		pr_info("interrupted");
 		return -ERESTARTSYS;
 	}
+
 	dma_unmap_single(&gpu->pdev->dev, dma_addr, count, DMA_TO_DEVICE);
+	unpin(pages, pages_pinned);
 	kfree(kbuf);
-	kvfree(pages);
 	return count;
 }
 
