9e6c7b7aaf69c6850398c3eb2e645e60755d8ce4
David Ventura <davidventura27@gmail.com>
2024-05-27 17:54:24 +0200

    test writing to gop

--- OptionRom/rom.c
+++ OptionRom/rom.c
@@ -8,11 +8,29 @@ EFI_STATUS EFIAPI OptionRomEntry (
   IN  EFI_SYSTEM_TABLE  *SystemTable
   ) {
     (void)ImageHandle;
-    (void)SystemTable;
+	EFI_BOOT_SERVICES *gBS = SystemTable->BootServices;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL *Gop;
+	EFI_STATUS Status;
+
+	Status = gBS->LocateProtocol(&gEfiGraphicsOutputProtocolGuid, NULL, (VOID**)&Gop);
+	if (EFI_ERROR(Status)) {
+		return Status;
+	}
+
+	Gop->Blt(
+			Gop,
+			(EFI_GRAPHICS_OUTPUT_BLT_PIXEL *)&(EFI_GRAPHICS_OUTPUT_BLT_PIXEL){0x00, 0xFF, 0xFF, 0x00}, // Blue color
+			EfiBltVideoFill,
+			0, 0, // Source coordinates
+			0, 0, // Destination coordinates
+			Gop->Mode->Info->HorizontalResolution, Gop->Mode->Info->VerticalResolution, // Width and Height
+			0 // Delta
+			);
 
 	// vvv this one does nothing
 	//
-    DEBUG((EFI_D_INFO, "DDDMyOptionRom loaded\n"));
+    DEBUG ((EFI_D_INFO, "DDDMyOptionRom loaded\n"));
+    DEBUG ((EFI_D_ERROR, "EEEMyOptionRom loaded\n"));
 	// vvv this one works
     Print(L"PPMyOptionRom loaded\n");
 
